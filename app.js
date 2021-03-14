65044539d81d0a2d76b9f6d10ede67a6a23ae26f
Bob Huo <bob83080@gmail.com>
2021-03-14 23:04:18 +0800

    feat: add logout feature

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 
+const usePassport = require('./config/passport')
+
 require('./config/mongoose')
 
 // --------路由設定-------- //
@@ -16,6 +18,8 @@ app.set('view engine', 'handlebars')
 app.use((bodyParser.urlencoded({ extended: true })))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(session({
   secret: 'ThisIsMySecret',
