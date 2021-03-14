38d37fc24cbe2a87e26b6c7cad5c0395782aa3e2
Bob Huo <bob83080@gmail.com>
2021-03-14 22:14:12 +0800

    login assignment start

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')
