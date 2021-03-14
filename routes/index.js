9403e7ce9097e88bc9842fee75964534f7717feb
Bob Huo <bob83080@gmail.com>
2021-03-14 22:33:25 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ const restaurant = require('./modules/restaurant')
 const search = require('./modules/search')
 const create = require('./modules/create')
 const sort = require('./modules/sort')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurant', restaurant)
 router.use('/search', search)
 router.use('/new', create)
 router.use('/sort', sort)
+router.use('/users', users)
 
 module.exports = router
